9ecc7c88995fb699769cd1c16adb21c2e5da3db2
DaviNunes05 <davi.nunes05@gmail.com>
2023-05-19 20:43:06 -0300

    Está com erro na hora de buscar os Posts.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import { useAuthentication } from './hooks/useAuthentication';
 
 import { AuthPovider } from './context/AuthContext';
 
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './pages/Login/Login';
@@ -41,7 +41,7 @@ function App() {
           <Navbar />
           <div className="container">
             <Routes>
-              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
               <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
